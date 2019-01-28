bfa21c4f0270a43cb230ae6d2f176e51c17beb35
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-01-28 21:42:58 +0000

    first commit

---
+++ lib/career_profiles.rb
@@ -0,0 +1,6 @@
+require "career_profiles/version"
+
+module CareerProfiles
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/career_profiles/version.rb
@@ -0,0 +1,3 @@
+module CareerProfiles
+  VERSION = "0.1.0"
+end
