487f6a2265437338f83923fc289c2f401c3d1511
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-01-31 02:49:25 +0000

    add occupation attributes

--- lib/career_profiles/scraper.rb
+++ lib/career_profiles/scraper.rb
@@ -20,7 +20,7 @@ class CareerProfiles::Scraper
       end
       urls.each do |u|
         u.insert(0,"http://www.careerprofiles.info/")
-    end
+      end
 
     careers = []
     names.each {|name| careers << {:name => name}}
@@ -34,15 +34,64 @@ class CareerProfiles::Scraper
   end
 
   def self.scrape_career_page_index(link)
-    get_career_page(link).css("div.mainContent table td")
+    get_career_page(link).css("div.mainContent table td a")
   end
 
   def self.scrape_occupations(link)
     names = []
-    scrape_career_page_index(link).each do |o|
-      names << o.css("a").text
+    scrape_career_page_index(link).each do |n|
+      names << n.text
     end
     names.delete("")
 
+    urls = []
+    scrape_career_page_index(link).each do |u|
+      urls << u['href']
+    end
+    urls.each do |u|
+      u.insert(0,"http://www.careerprofiles.info/")
+    end
+
+    summary_index = get_career_page(link).css("div.mainContent table")
+    summaries = []
+    i = 1
+    summary_index.each do |s|
+      si = s.css("tr")[i]
+      summaries << si.css("td")[1].text
+      i += 1
+    end
+
+    educations = []
+    i = 1
+    summary_index.each do |s|
+      educations << s.css("tr")[i].css("td")[2].text
+      i += 1
+    end
+    median_pays = []
+    i = 1
+    summary_index.each do |s|
+      median_pays << s.css("tr")[i].css("td")[3].text
+      i += 1
+    end
+
+
+    occupations = []
+    names.each {|name| occupations << {:name => name}}
+    u = 0
+    urls.each {|url|  occupations[u][:url] = url; u += 1}
+    s = 0
+    summaries.each {|summary| occupations[s][:summary] = summary; s += 1}
+    e = 0
+    educations.each {|education| occupations[e][:education] = education; e += 1}
+    mp = 0
+    median_pays.each{|median_pay| occupations[mp][:median_pay_2018] = median_pay; mp += 1}
+    occupations
   end
+
+
+  def self.add_attributes_to_occupations(link)
+    summary_index = get_career_page(link)
+  end
+
+
 end
